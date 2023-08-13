6f71bb5178ee0b4fcc6242618ccb0f47413ab233
Abdallh Elkahlout <89447126+abdallh-elkahlout@users.noreply.github.com>
2023-08-13 13:39:52 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -17,7 +17,17 @@ let logo = document.querySelector(".logo");
 let images = ["img1.jpg", "img2.jpg"];
 
 setInterval(() => {
-  let randomNum = Math.floor(Math.random() * 2);
+  let randomNum = Math.floor(Math.random() * images.length);
   landingPage.style.backgroundImage =
     'url("../imgs/' + images[randomNum] + '")';
+  if (landingPage.style.backgroundImage == 'url("../imgs/img1.jpg")') {
+    introH1.style.color = "black";
+    introH1Span.style.color = "#72791f";
+    logo.style.color = "white";
+    desc.style.color = "white";
+  } else{
+    introH1.style.color = "#3f3f38";
+    introH1Span.style.color = "#7c4c4c";
+    desc.style.color = "#3f3f38";
+  }
 }, 2000);
