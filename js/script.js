b7f5d3a0340ea9d143fbf3d2497686747edf8b50
Abdallh Elkahlout <89447126+abdallh-elkahlout@users.noreply.github.com>
2023-08-13 13:18:59 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// toggle soin
+
+document.querySelector(".setting-box .toggle-setting .fa-gear").onclick =
+  function () {
+    this.classList.toggle("fa-spin");
+    document.querySelector(".setting-box").classList.toggle("open");
+  };
+
 //select landing page image
 
 let landingPage = document.querySelector(".landing-page");
